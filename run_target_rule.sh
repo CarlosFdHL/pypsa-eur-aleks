61cea743a2b7836d9c6f85421e829690e606b921
CarlosFdHL <carlosfdhl@gmail.com>
2026-09-04 10:36:16 +0200

    config files and .sh

--- run_target_rule.sh
+++ run_target_rule.sh
@@ -2,6 +2,7 @@
 source ~/miniforge3/condabin/conda
 
 CONFIG="config/tsam-test/consecutive_years_segmented.yaml"
+TARGET="resources/consecutive_years_seg/weather_years_1941_1986_2010_9h/networks/base_s.nc"
 
 # Extract prefix from config and set up Slurm log directory
 PREFIX=$(python -c "import yaml; c=yaml.safe_load(open('$CONFIG')); print(c['run']['prefix'])")
@@ -9,5 +10,4 @@ export SLURM_LOG_DIR="slurm_logs/$PREFIX"
 mkdir -p "$SLURM_LOG_DIR"
 echo "Slurm logs will be stored in: $SLURM_LOG_DIR"
 
-./snakemake_prepare_sector_network --configfile="$CONFIG" --jobs=100
-./snakemake_solve_thin --configfile="$CONFIG" --keep-going --jobs=15
+snakemake "$TARGET" --configfile="$CONFIG" --jobs=8
