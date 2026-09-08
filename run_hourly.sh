fdb3b3f5f8ffc9d88f8858a3f779278626630c03
CarlosFdHL <carlosfdhl@gmail.com>
2026-09-08 12:19:23 +0200

    configs and .sh files

--- run_hourly.sh
+++ run_hourly.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source ~/miniforge3/condabin/conda
 
-CONFIG="config/tsam-test/consecutive_years_segmented.yaml"
+# CONFIG="config/tsam-test/consecutive_years_segmented.yaml"
+CONFIG="config/tsam-test/hourly.yaml"
 
 # Extract prefix from config and set up Slurm log directory
 PREFIX=$(python -c "import yaml; c=yaml.safe_load(open('$CONFIG')); print(c['run']['prefix'])")
@@ -10,4 +11,4 @@ mkdir -p "$SLURM_LOG_DIR"
 echo "Slurm logs will be stored in: $SLURM_LOG_DIR"
 
 ./snakemake_prepare_sector_network --configfile="$CONFIG" --jobs=100
-./snakemake_solve_thin --configfile="$CONFIG" --keep-going --jobs=15
+./snakemake_solve_thin --configfile="$CONFIG" --keep-going --jobs=1
--- run_target_rule.sh
+++ run_target_rule.sh
@@ -2,7 +2,7 @@
 source ~/miniforge3/condabin/conda
 
 CONFIG="config/tsam-test/consecutive_years_segmented.yaml"
-TARGET="resources/consecutive_years_seg/weather_years_1941_1986_2010_9h/networks/base_s.nc"
+TARGET="resources/consecutive_years_seg2/weather_years_1941_1986_2010_9h/snapshot_weightings_base_s_50_elec__.csv"
 
 # Extract prefix from config and set up Slurm log directory
 PREFIX=$(python -c "import yaml; c=yaml.safe_load(open('$CONFIG')); print(c['run']['prefix'])")
@@ -10,4 +10,4 @@ export SLURM_LOG_DIR="slurm_logs/$PREFIX"
 mkdir -p "$SLURM_LOG_DIR"
 echo "Slurm logs will be stored in: $SLURM_LOG_DIR"
 
-snakemake "$TARGET" --configfile="$CONFIG" --jobs=8
+snakemake "$TARGET" --configfile="$CONFIG" --jobs=10 #--rerun-triggers mtime
